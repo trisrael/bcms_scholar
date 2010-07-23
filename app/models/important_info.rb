29d27ee16b0cb5ccf16ef5116f03fb94fca32e58
Your Name <tgoffman@gmail.com>
2010-07-23 06:04:31 -0400

    update important info, and styles for CMS workflow with newly create containers

--- app/models/important_info.rb
+++ app/models/important_info.rb
@@ -1,3 +1,6 @@
 class ImportantInfo < ActiveRecord::Base
   acts_as_content_block :taggable => true
+  validates_presence_of :title
+  validates_presence_of :text
+  validates_presence_of :name
 end	
