b23d06e31ea7cbec05d856b808565b577d99f9d1
Your Name <tgoffman@gmail.com>
2010-08-03 02:13:40 -0400

    tweak new features portles, change styling of updates

--- app/models/update.rb
+++ app/models/update.rb
@@ -1,5 +1,6 @@
 class Update < ActiveRecord::Base
   acts_as_content_block :taggable => true, :versioned => false
   belongs_to :student
-  validates_presence_of :student_id, :update_text, :name
+  validates_presence_of :student_id, :update_text, :name 
+  can_tag
 end
---
+++ app/portlets/updates_portlet.rb
@@ -0,0 +1,10 @@
+class UpdatesPortlet < Portlet
+  validates_presence_of :tag_list, :name
+  # Mark this as 'true' to allow the portlet's template to be editable via the CMS admin UI.
+  enable_template_editor false
+     
+  def render
+    @updates = Update.with_tag(self.tag_list)
+  end
+    
+end
