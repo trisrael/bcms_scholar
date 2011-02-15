b9ad565433c92cf1ba18c2105686b877c1b6aa7a
Geneen Georgiev <admin@bcmsddcscholar.(none)>
2011-02-15 11:35:05 +0000

    remove config.gem calls due to addtionof bundler

--- config/environment.rb
+++ config/environment.rb
@@ -6,23 +6,17 @@
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 Rails::Initializer.run do |config|
-  config.gem 'browsercms' 
-  config.gem 'bcms_settings'
-  config.gem 'bcms_seo_sitemap' 
-  config.gem "bcms_fckeditor"
-#Comment out until the gem is installed correctly
-#  config.gem 'bcms_lb_photo_gallery', :version => '1.0.0'
 
   # Settings in config/environments/* take precedence over those specified here.
   # Application configuration should go into files in config/initializers
   # -- all .rb files in that directory are automatically loaded.
 
   # Add additional load paths for your own custom dirs
-  # config.load_paths += %W( #{RAILS_ROOT}/vendor/gems )
+#   config.load_paths += %W( #{RAILS_ROOT}/vendor/gems )
   # config.gem 'bcms_content_syncing'
-  config.load_paths += Dir["#{RAILS_ROOT}/vendor/gems/**"].map do |dir| 
-    File.directory?(lib = "#{dir}/lib") ? lib : dir
-  end
+#  config.load_paths += Dir["#{RAILS_ROOT}/vendor/gems/**"].map do |dir| 
+ #   File.directory?(lib = "#{dir}/lib") ? lib : dir
+ # end
     
   # Specify gems that this application depends on and have them installed with rake gems:install
   # config.gem "bj"
